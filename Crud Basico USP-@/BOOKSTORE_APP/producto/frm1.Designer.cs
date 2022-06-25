e9f99d54d59cd147277030dd72fc2c2c9fc90909
MCFigue <daniel.j.f.m@hotmail.com>
2022-06-25 18:50:51 -0500

    subida de clase 2

---
+++ Crud Basico USP-@/BOOKSTORE_APP/producto/frm1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace productos
+{
+    partial class frm1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm1));
+            this.primaryGrid = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cliente = new System.Windows.Forms.TextBox();
+            this.idpublicacion = new System.Windows.Forms.TextBox();
+            this.cantidad1 = new System.Windows.Forms.Label();
+            this.cantidad = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dcto = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboEmpleados = new System.Windows.Forms.ComboBox();
+            this.txtarea = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.primaryGrid)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // primaryGrid
+            // 
+            this.primaryGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.primaryGrid.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.primaryGrid.BackgroundColor = System.Drawing.Color.White;
+            this.primaryGrid.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.Disable;
+            this.primaryGrid.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(32)))), ((int)(((byte)(52)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Yu Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.primaryGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.primaryGrid.ColumnHeadersHeight = 30;
+            this.primaryGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.primaryGrid.EnableHeadersVisualStyles = false;
+            this.primaryGrid.GridColor = System.Drawing.Color.DimGray;
+            this.primaryGrid.Location = new System.Drawing.Point(3, 0);
+            this.primaryGrid.Name = "primaryGrid";
+            this.primaryGrid.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(32)))), ((int)(((byte)(52)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Yu Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.primaryGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.primaryGrid.RowHeadersVisible = false;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(32)))), ((int)(((byte)(52)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Yu Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            this.primaryGrid.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.primaryGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.primaryGrid.Size = new System.Drawing.Size(916, 642);
+            this.primaryGrid.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.primaryGrid);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(919, 642);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(940, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Cliente";
+            // 
+            // cliente
+            // 
+            this.cliente.Font = new System.Drawing.Font("Yu Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cliente.Location = new System.Drawing.Point(944, 89);
+            this.cliente.Name = "cliente";
+            this.cliente.Size = new System.Drawing.Size(335, 28);
+            this.cliente.TabIndex = 7;
+            this.cliente.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // idpublicacion
+            // 
+            this.idpublicacion.Font = new System.Drawing.Font("Yu Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idpublicacion.Location = new System.Drawing.Point(944, 227);
+            this.idpublicacion.Name = "idpublicacion";
+            this.idpublicacion.Size = new System.Drawing.Size(335, 28);
+            this.idpublicacion.TabIndex = 11;
+            this.idpublicacion.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.idpublicacion.TextChanged += new System.EventHandler(this.idpublicacion_TextChanged);
+            // 
+            // cantidad1
+            // 
+            this.cantidad1.AutoSize = true;
+            this.cantidad1.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cantidad1.Location = new System.Drawing.Point(940, 287);
+            this.cantidad1.Name = "cantidad1";
+            this.cantidad1.Size = new System.Drawing.Size(70, 20);
+            this.cantidad1.TabIndex = 12;
+            this.cantidad1.Text = "Cantidad";
+            // 
+            // cantidad
+            // 
+            this.cantidad.Font = new System.Drawing.Font("Yu Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cantidad.Location = new System.Drawing.Point(944, 310);
+            this.cantidad.Name = "cantidad";
+            this.cantidad.Size = new System.Drawing.Size(335, 28);
+            this.cantidad.TabIndex = 13;
+            this.cantidad.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Blue;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Yu Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(944, 446);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 37);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Grabar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dcto
+            // 
+            this.dcto.Font = new System.Drawing.Font("Yu Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dcto.Location = new System.Drawing.Point(944, 387);
+            this.dcto.Name = "dcto";
+            this.dcto.Size = new System.Drawing.Size(335, 28);
+            this.dcto.TabIndex = 17;
+            this.dcto.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(945, 364);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 20);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Descuento";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(940, 134);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 20);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "Empleado";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(940, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 20);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Código de Publicación";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Crimson;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button2.FlatAppearance.BorderSize = 2;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Yu Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(1128, 446);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 37);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "Salir";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(1004, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(241, 20);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "REGISTRAR VENTA \"BOOK STORE\"";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // cboEmpleados
+            // 
+            this.cboEmpleados.FormattingEnabled = true;
+            this.cboEmpleados.IntegralHeight = false;
+            this.cboEmpleados.ItemHeight = 13;
+            this.cboEmpleados.Items.AddRange(new object[] {
+            "1 - AGUERO RAMOS",
+            "2 - SANCHEZ ROMERO",
+            "3 - LUNG WON",
+            "4 - CASTILLO RAMOS",
+            "5 - MILICICH FLORES",
+            "6 -DELGADO BARRERA",
+            "7 -GARCIA SOLIS"});
+            this.cboEmpleados.Location = new System.Drawing.Point(944, 171);
+            this.cboEmpleados.Name = "cboEmpleados";
+            this.cboEmpleados.Size = new System.Drawing.Size(330, 21);
+            this.cboEmpleados.TabIndex = 27;
+            // 
+            // txtarea
+            // 
+            this.txtarea.Location = new System.Drawing.Point(949, 552);
+            this.txtarea.Multiline = true;
+            this.txtarea.Name = "txtarea";
+            this.txtarea.Size = new System.Drawing.Size(330, 46);
+            this.txtarea.TabIndex = 28;
+            // 
+            // frm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1301, 642);
+            this.Controls.Add(this.txtarea);
+            this.Controls.Add(this.cboEmpleados);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dcto);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cantidad);
+            this.Controls.Add(this.cantidad1);
+            this.Controls.Add(this.idpublicacion);
+            this.Controls.Add(this.cliente);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frm1";
+            this.Text = "CRUD REGISTRAR VENTA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.primaryGrid)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView primaryGrid;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox cliente;
+        private System.Windows.Forms.TextBox idpublicacion;
+        private System.Windows.Forms.Label cantidad1;
+        private System.Windows.Forms.TextBox cantidad;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox dcto;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboEmpleados;
+        private System.Windows.Forms.TextBox txtarea;
+    }
+}
+
---
+++ Crud Basico USP-@/BOOKSTORE_APP/producto/frm1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace productos
+{
+    public partial class frm1 : Form
+    {
+        string server = "Data Source =LAPTOP-T7UOU22V\\SQLEXPRESS; Initial Catalog=BOOKSTORE; Integrated Security = True";
+        SqlConnection conectar = new SqlConnection();
+        Conexion con = new Conexion();
+        public frm1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string emple = (string)cboEmpleados.SelectedItem;
+            //int idempleadow = Convert.ToInt32(emple);
+
+            int b = cboEmpleados.SelectedIndex;
+            int idempleadoc = b + 1;
+
+            
+            //int idempleadoc = Convert.ToInt32(idempleado.Text);
+            //double money = Convert.ToDouble(preciode.Text);
+            int cant = Convert.ToInt32(cantidad.Text);
+            double descuento = Convert.ToDouble(dcto.Text);
+            //double subtotale = Convert.ToDouble(subtotal.Text);
+            //double impuestoe = Convert.ToDouble(impuesto.Text);
+            conectar.ConnectionString = server;
+            conectar.Open();
+            SqlCommand cmd = new SqlCommand("USP_REGISTRAR_VENTA", conectar);
+            cmd.CommandType = CommandType.StoredProcedure;
+            //cmd.Parameters.AddWithValue("@IDVENTA", 0);
+            cmd.Parameters.AddWithValue("@CLIENTE", cliente.Text);
+            cmd.Parameters.AddWithValue("@IDEMPLEADO",idempleadoc );
+            cmd.Parameters.AddWithValue("@IDPUBLICACION", idpublicacion.Text);
+            cmd.Parameters.AddWithValue("@CANTIDAD", cant);
+            //cmd.Parameters.AddWithValue("@PRECIODE",0);
+            cmd.Parameters.AddWithValue("@DCTO", descuento);
+            //cmd.Parameters.AddWithValue("@SUBOTOTAL", 0);
+            //cmd.Parameters.AddWithValue("@IMPUESTO", 0);
+            cmd.Parameters.AddWithValue("@CODIGO", 0);
+            cmd.Parameters.AddWithValue("@MENSAJE", "0");
+
+            txtarea.Text = "La venta se registro exitosamente \n";
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException EX)
+            {
+                MessageBox.Show(EX.ToString());
+                throw;
+            }
+            con.mostrar("venta", primaryGrid);
+            conectar.Close();
+
+        }
+
+        private void BORRAR_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            con.mostrar("productos",primaryGrid);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void idpublicacion_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            this.Hide();
+
+        }
+       
+    }
+}
